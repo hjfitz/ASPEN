fa5e31cc9486cb4d5112a10a365ea1db70a9d855
Harry <up780461@myport.ac.uk>
2019-02-17 18:08:49 +0000

    param passthrough working. todo: login/create form and relevant APIs

--- src/client/login/index.jsx
+++ src/client/login/index.jsx
@@ -1,7 +1,39 @@
-import {h, render} from 'preact'
+import {h, render, Component} from 'preact'
 
-const App = () => (
-	<h1>Hello, Login!</h1>
-)
+import 'materialize-css/sass/materialize.scss'
 
+
+class App extends Component {
+	constructor() {
+		super()
+		const loginUrl = window.location.href.split('google_redir=')[1]
+		console.log(loginUrl)
+		this.state = {
+			loginUrl,
+			form: 'login',
+		}
+	}
+
+	componentWillMount() {
+
+	}
+
+	render() {
+		return (
+			<div className="container">
+				<div className="row">
+					<div className="col s12">
+						<div className="card">
+							<div className="card-content">
+								<span className="card-title">Login</span>
+								<a className="waves-effect waves-light btn white blue-text text-darken-1" href={this.state.loginUrl}>Login with Google</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		)
+	}
+}
 render(<App />, document.querySelector('[preact-root]'))
+require('preact/debug')
